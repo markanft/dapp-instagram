0d1519efa14e7d12394e94a2bbd2432164b7aeca
Marc Andrés Fontanet <markanft@gmail.com>
2021-11-03 15:49:26 +0100

    update frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,178 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import Identicon from 'identicon.js';
+import detectEthereumProvider from '@metamask/detect-provider';
 import './App.css';
 import Dappinstagram from '../abis/Dappinstagram.json';
 import Navbar from './Navbar';
 import Main from './Main';
+import { Buffer } from 'buffer';
+import ipfsClient from 'ipfs-http-client';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: '',
+const ipfs = ipfsClient({
+  host: 'ipfs.infura.io',
+  port: 5001,
+  protocol: 'https',
+});
+
+function App() {
+  const [account, setAccount] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [dappinstagram, setDappinstagram] = useState(null);
+  const [images, setImages] = useState([]);
+  const [buffer, setBuffer] = useState();
+  let chainId;
+
+  const startApp = async provider => {
+    // If the provider returned by detectEthereumProvider is not the same as
+    // window.ethereum, something is overwriting it, perhaps another wallet.
+    if (provider !== window.ethereum) {
+      console.error('Do you have multiple wallets installed?');
+    }
+
+    const web3 = new Web3(window.ethereum);
+    const networkId = await web3.eth.net.getId();
+    const networkData = Dappinstagram.networks[networkId];
+    if (networkData) {
+      const dappinstagram = new web3.eth.Contract(Dappinstagram.abi, networkData.address);
+      setDappinstagram(dappinstagram);
+      const imageCount = await dappinstagram.methods.imageCount().call();
+
+      let imagesArray = [];
+      for (let i = 1; i <= imageCount; i++) {
+        const image = await dappinstagram.methods.images(i).call();
+        imagesArray.push(image);
+      }
+
+      setImages(imagesArray.sort((a, b) => b.tipAmount - a.tipAmount));
+    } else {
+      window.alert('Dappinstagram contract not deployed to detected network');
+    }
+    // Access the decentralized web!
+  };
+
+  const handleChainChanged = _chainId => {
+    // We recommend reloading the page, unless you must do otherwise
+    if (chainId !== _chainId) {
+      window.location.reload();
+      // Run any other necessary logic...
+    }
+    // console.log(_chainId);
+    // window.location.reload();
+  };
+
+  let currentAccount = null;
+  const handleAccountsChanged = accounts => {
+    if (accounts.length === 0) {
+      setAccount(null);
+      // MetaMask is locked or the user has not connected any accounts
+      console.log('Please connect to MetaMask.');
+    } else if (accounts[0] !== currentAccount) {
+      currentAccount = accounts[0];
+      setAccount(currentAccount);
+      // Do any other work!
+    }
+  };
+
+  let loadWeb3 = async () => {
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      await startApp(provider); // Initialize your app
+    } else {
+      console.log('Please install MetaMask!');
+    }
+
+    window.ethereum
+      .request({ method: 'eth_accounts' })
+      .then(handleAccountsChanged)
+      .catch(err => {
+        // Some unexpected error.
+        // For backwards compatibility reasons, if no accounts are available,
+        // eth_accounts will return an empty array.
+        console.error(err);
+      });
+
+    chainId = await window.ethereum.request({ method: 'eth_chainId' }).catch(err => console.error(err));
+
+    window.ethereum.on('chainChanged', handleChainChanged);
+    window.ethereum.on('accountsChanged', handleAccountsChanged);
+  };
+
+  const signIn = () => {
+    window.ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then(handleAccountsChanged)
+      .catch(err => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+  };
+
+  const captureFile = event => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onloadend = () => {
+      setBuffer(Buffer(reader.result));
     };
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        {this.state.loading ? (
-          <div id="loader" className="text-center mt-5">
-            <p>Loading...</p>
-          </div>
-        ) : (
-          <Main
-          // Code...
-          />
-        )}
-      </div>
-    );
-  }
+  const uploadImage = description => {
+    console.log('Submiting dile to ipfs');
+
+    ipfs
+      .add(buffer)
+      .then(result => {
+        console.log('Ipfs result', result);
+        setLoading(true);
+        dappinstagram.methods
+          .uploadImage(result[0].hash, description)
+          .send({ from: account })
+          .on('transactionHash', hash => {
+            setLoading(false);
+          });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const tipImageOwner = async (id, tipAmount) => {
+    setLoading(true);
+    dappinstagram.methods
+      .tipImageOwner(id)
+      .send({ from: account, value: tipAmount })
+      .on('transactionHash', hash => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    loadWeb3();
+    setLoading(false);
+  }, []);
+
+  return (
+    <div>
+      <Navbar account={account} onSignIn={signIn} />
+      {loading ? (
+        <div id="loader" className="text-center mt-5">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <>
+          <Main captureFile={captureFile} uploadImage={uploadImage} images={images} tipImageOwner={tipImageOwner} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,90 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
+import web3 from 'web3';
 
-class Main extends Component {
+console.log(window.ethereum);
+function Main(props) {
+  let imageDescription;
 
-  render() {
-    return (
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
-            <div className="content mr-auto ml-auto">
-              <p>&nbsp;</p>
-              <h1 className="d-4">Edit this file in App.js!</h1>
-                
-                {/* Code ... */}
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+          <div className="content mr-auto ml-auto">
+            <p>&nbsp;</p>
+            <h2>Share Image</h2>
+            <form
+              onSubmit={event => {
+                event.preventDefault();
+                const description = imageDescription.value;
+                props.uploadImage(description);
+              }}
+            >
+              <input type="file" accept=".jpg, .jpeg, .png, .bmp, .gif" onChange={props.captureFile} />
+              <div className="form-group mr-sm-2">
+                <br></br>
+                <input
+                  id="imageDescription"
+                  type="text"
+                  ref={input => {
+                    imageDescription = input;
+                  }}
+                  className="form-control"
+                  placeholder="Image description..."
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary btn-block btn-lg">
+                Upload!
+              </button>
+            </form>
 
-              <p>&nbsp;</p>
-                
-                {/* Code ... */}
+            <p>&nbsp;</p>
 
-            </div>
-          </main>
-        </div>
+            {props.images.map((image, key) => {
+              return (
+                <div className="card mb-4" key={key}>
+                  <div className="card-header">
+                    <img
+                      className="mr-2"
+                      width="30"
+                      height="30"
+                      src={`data:image/png;base64,${new Identicon(image.author, 30).toString()}`}
+                    />
+                    <small className="text-muted">{image.author}</small>
+                  </div>
+                  <ul id="imageList" className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      <p className="text-center">
+                        <img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px' }} />
+                      </p>
+                      <p>{image.description}</p>
+                    </li>
+                    <li key={key} className="list-group-item py-2">
+                      <small className="float-left mt-1 text-muted">
+                        TIPS: {web3.utils.fromWei(image.tipAmount.toString(), 'Ether')} ETH
+                      </small>
+                      <button
+                        className="btn btn-link btn-sm float-right pt-0"
+                        name={image.id}
+                        onClick={event => {
+                          let tipAmount = web3.utils.toWei('0.1', 'Ether');
+                          console.log(event.target.name, tipAmount);
+                          props.tipImageOwner(event.target.name, tipAmount);
+                        }}
+                      >
+                        TIP 0.1 ETH
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Main;
