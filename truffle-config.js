9319537465daa5d3e00234d3a48d629a23a85d66
Marc Andrés Fontanet <markanft@gmail.com>
2021-11-04 10:32:14 +0100

    add provider for rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,20 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = '';
 
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 7545,
-      network_id: '*', // Match any network id
+      network_id: '5777', // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, '');
+      },
+      network_id: 4,
     },
   },
   contracts_directory: './src/contracts/',
